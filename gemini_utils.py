684cc146f2cd89b7254d5d268e41cf03dd619531
jake-seunghyukoh <jake@seunghyukoh.com>
2024-05-17 14:00:50 +0900

    chore: Update logging format

--- gemini_utils.py
+++ gemini_utils.py
@@ -1,5 +1,5 @@
+import logging
 import os
-from logging import getLogger
 from typing import List
 
 import google.generativeai as genai
@@ -7,7 +7,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-logger = getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
 
 
 def complete(
